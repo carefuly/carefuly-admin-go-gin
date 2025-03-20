9fd3e034fb0f5e0d5b19c5bd1093707af60b60b8
CJiaの用心 <2224693191@qq.com>
2025-03-20 22:13:49 +0800

    3.20 初始化项目和公共模型字段

---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/carefuly/carefuly-admin-go-gin
+
+go 1.23
+
+require (
+	github.com/satori/go.uuid v1.2.0
+	gorm.io/gorm v1.25.12
+)
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+)
---
+++ pkg/models/models.go
@@ -0,0 +1,37 @@
+/**
+ * Description：
+ * FileName：models.go
+ * Author：CJiaの用心
+ * Create：2025/3/19 22:50:16
+ * Remark：
+ */
+
+package models
+
+import (
+	uuid "github.com/satori/go.uuid"
+	"gorm.io/gorm"
+	"time"
+)
+
+// CoreModels 公共模型
+// 核心标准抽象模型模型,可直接继承使用
+// 增加审计字段, 覆盖字段时, 字段名称请勿修改, 必须统一审计字段名称
+type CoreModels struct {
+	Id         string     `gorm:"type:varchar(100);primary_key;column:id;comment:主键ID" json:"id"`      // 主键ID
+	Sort       int        `gorm:"type:int;default:1;column:sort;comment:显示排序" json:"sort"`             // 显示排序
+	Creator    string     `gorm:"type:varchar(100);index;column:creator;comment:创建人" json:"creator"`   // 创建人
+	Modifier   string     `gorm:"type:varchar(100);index;column:modifier;comment:修改人" json:"modifier"` // 修改人
+	BelongDept int        `gorm:"type:int;index;column:belong_dept;comment:数据归属部门" json:"belongDept"`  // 数据归属部门
+	Status     bool       `gorm:"type:bool;index;default:true;column:status;comment:状态" json:"status"` // 状态
+	CreateTime *time.Time `gorm:"autoCreateTime;column:create_time;comment:创建时间" json:"-"`             // 创建时间
+	UpdateTime *time.Time `gorm:"autoUpdateTime;column:update_time;comment:修改时间" json:"-"`             // 修改时间
+	Remark     string     `gorm:"type:varchar(255);column:remark;comment:备注" json:"remark"`            // 备注
+}
+
+func (c *CoreModels) BeforeCreate(tx *gorm.DB) (err error) {
+	// 记录ID
+	c.Id = uuid.NewV4().String()
+	// 返回异常
+	return
+}
